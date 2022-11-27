9f404aea5ec6072fc96f458a4b9f17105a52b189
Mike Vercoelen <mike.vercoelen@gmail.com>
2022-11-27 12:25:45 +0100

    Clean up

--- packages/stadium/src/index.ts
+++ packages/stadium/src/index.ts
@@ -2,10 +2,47 @@ interface IStadiumConfig {
   appId: string
 }
 
+interface IStadiumHandlers {
+  onConnected?: (socket: WebSocket) => void
+  onDisconnected?: (socket: WebSocket) => void
+}
+
 export class Stadium {
   private readonly config: IStadiumConfig
+  private readonly handlers: IStadiumHandlers
+  private isConnected = false
+  private url: string
+  public socket?: WebSocket
 
-  constructor (config: IStadiumConfig) {
+  constructor (config: IStadiumConfig, handlers: IStadiumHandlers) {
     this.config = config
+    this.handlers = handlers
+
+    // TODO: add env var
+    this.url = 'ws://localhost:5000'
+  }
+
+  public async connect () {
+    if (this.isConnected) {
+      return
+    }
+
+    this.socket = new WebSocket(this.url)
+
+    this.socket.addEventListener('open', () => {
+      this.isConnected = true
+
+      if (this.handlers.onConnected) {
+        this.handlers.onConnected(this.socket!)
+      }
+    })
+
+    this.socket.addEventListener('close', () => {
+      this.isConnected = false
+
+      if (this.handlers.onDisconnected) {
+        this.handlers.onDisconnected(this.socket!)
+      }
+    })
   }
 }
