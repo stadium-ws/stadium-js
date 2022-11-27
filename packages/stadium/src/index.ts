c5bb134188e151217b40b7202b29d4240d92e1dc
Mike Vercoelen <mike.vercoelen@gmail.com>
2022-11-27 12:43:17 +0100

    Clean up

--- packages/stadium/src/index.ts
+++ packages/stadium/src/index.ts
@@ -1,5 +1,9 @@
+import fetch from 'cross-fetch'
+
+// TODO: use cross platform websockets i.e. @see https://github.com/heineiuo/isomorphic-ws
+
 interface IStadiumConfig {
-  appId: string
+  apiKey: string
 }
 
 interface IStadiumHandlers {
@@ -11,7 +15,8 @@ export class Stadium {
   private readonly config: IStadiumConfig
   private readonly handlers: IStadiumHandlers
   private isConnected = false
-  private url: string
+  private socketUrl: string
+  private apiUrl: string
   public socket?: WebSocket
 
   constructor (config: IStadiumConfig, handlers: IStadiumHandlers) {
@@ -19,7 +24,8 @@ export class Stadium {
     this.handlers = handlers
 
     // TODO: add env var
-    this.url = 'ws://localhost:5000'
+    this.socketUrl = 'ws://localhost:5000'
+    this.apiUrl = 'http://localhost:4000'
   }
 
   public async connect () {
@@ -27,7 +33,7 @@ export class Stadium {
       return
     }
 
-    this.socket = new WebSocket(this.url)
+    this.socket = new WebSocket(this.socketUrl)
 
     this.socket.addEventListener('open', () => {
       this.isConnected = true
@@ -44,5 +50,58 @@ export class Stadium {
         this.handlers.onDisconnected(this.socket!)
       }
     })
+  }
+
+  public async getAppAccessToken (apiKey: string, apiSecret: string): Promise<{
+    accessToken: string
+    expiresIn: string
+    tokenType: string
+  }> {
+    const url = `${this.apiUrl}/oauth/token`
+
+    const res = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        grant_type: 'client_credentials',
+        client_id: apiKey,
+        client_secret: apiSecret
+      })
+    })
+
+    const data = await res.json() as {
+      access_token: string
+      expires_in: string
+      token_type: string
+    }
+
+    return {
+      accessToken: data.access_token,
+      expiresIn: data.expires_in,
+      tokenType: data.token_type
+    }
+  }
+
+  public async getUserAccessToken (userId: string, appAccessToken: string): Promise<{
+    accessToken: string
+    expiresIn: string
+  }> {
+    const url = `${this.apiUrl}/users/${userId}/auth`
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${appAccessToken}`
+      }
+    })
+
+    const data = await res.json() as {
+      token: string
+      expiresIn: string
+    }
+
+    return {
+      accessToken: data.token,
+      expiresIn: data.expiresIn
+    }
   }
 }
