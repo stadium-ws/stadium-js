30d1c57e8d86bac515aa3299d0a5456c86a7d0cb
Mike Vercoelen <mike.vercoelen@gmail.com>
2022-12-12 11:13:02 +0100

    Clean up

--- apps/demo-chat/src/views/Channel/Channel.tsx
+++ apps/demo-chat/src/views/Channel/Channel.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import Guard from '@/components/Guard'
+import useChannel from '@/hooks/useChannel'
+import { useRouter } from 'next/router'
+import Loader from '@/components/Loader'
+import styled from 'styled-components'
+
+const ChannelName = styled.div``
 
 const ChannelInner = () => {
+  const router = useRouter()
+  const channelId = router.query.id as string
+  const { loading, channel } = useChannel(channelId)
+
+  if (loading || !channel) {
+    return (
+      <Loader />
+    )
+  }
+
   return (
-    <div>
-      Channel goes here...
-    </div>
+    <>
+      <ChannelName>
+        {channel.name}
+      </ChannelName>
+    </>
   )
 }
 
