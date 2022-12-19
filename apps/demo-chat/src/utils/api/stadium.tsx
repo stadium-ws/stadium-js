bd7b2aa2075ac2edd049cb87e82ffa29fc451b71
Mike Vercoelen <mike.vercoelen@gmail.com>
2022-12-19 10:47:08 +0100

    Clean up

--- apps/demo-chat/src/utils/api/stadium.tsx
+++ apps/demo-chat/src/utils/api/stadium.tsx
@@ -1,8 +1,11 @@
 import { Stadium } from '@stadium-ws/core'
+import { API_URL, GATEWAY_URL } from '@/config'
 
 const stadium = new Stadium({
   clientId: '8Ax8/A34SJmycSLBAi8xFlTRT0BR8YJi6oBLr8qEvPc=',
-  clientSecret: '$2b$04$cy3A7evcUmC5xsSOQDu/KuevmBuz3K.XvtvqwLLvnuF9cmiSIhw0y'
+  clientSecret: '$2b$04$cy3A7evcUmC5xsSOQDu/KuevmBuz3K.XvtvqwLLvnuF9cmiSIhw0y',
+  apiUrl: API_URL,
+  gatewayUrl: GATEWAY_URL
 })
 
 export default stadium
